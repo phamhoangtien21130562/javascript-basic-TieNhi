30e84c52a331b14119128130b20e6febeba47bcd
Pham Hoang Tien <21130562@st.hcmuaf.edu.vn>
2024-04-23 14:13:43 +0700

    template-string,backtick:TieNhi

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -1,3 +1,16 @@
 // alert(`Hello World from helloWorld.js!`)
-let a = 5, b = 6, c = 7, d = 'abc';
-console.log(a + b + c+ d );
+// let a = 5, b = 6, c = 7, d = 'abc';
+// console.log(a + b + c+ d );
+
+// template string backtick ${}
+
+let a = 4;
+let b = 5;
+
+console.log(`a + b = ${a + b}`); // 9
+console.log(`Hinh chữ nhật 
+            Chiều dài  = ${a}
+            Chiều rộng = ${b}
+            Chu vi     = ${2*(a+b)}
+            Diện tích  = ${a*b}
+            `)
